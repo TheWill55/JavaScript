785c9335f174dd37ed8a793469aeebf1a37005f9
Wilson Alves <127157385+TheWill55@users.noreply.github.com>
2023-06-17 22:34:04 -0300

    aula 5

--- Aula01/script.js
+++ Aula01/script.js
@@ -1,7 +1,4 @@
-window.alert('Minha primeira menssagem')
+window.alert('Minha primeira menssagem')//Mostra uma mensagem 
+confirm('Esta gostando de JavaScrip?')//Janela que vai perguntar e mandar confirmar!
+prompt('Qual seu nome?')//vai mandar eu escrever algo e confirmar 
 
-confirm('Esta gostando de JavaScrip?')
-prompt('Qual seu nome?')
-
-const name = wilson
-window.alert
---
+++ teste.js
@@ -0,0 +1,10 @@
+//informações (idade,salário,sexo)
+var idade = 18 
+var salário = 2000.00
+var sexo = "M"
+
+//soma (n1 + n2)
+var n1 = 25
+var n2 = 45
+
+console.log(n1+n2)
